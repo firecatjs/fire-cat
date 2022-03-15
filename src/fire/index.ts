ad7ec33ad00ef40c582aec555139a7413c07bd94
韩少华 <millent@126.com>
2022-03-15 16:15:44 +0800

    remove error

--- src/fire/index.ts
+++ src/fire/index.ts
@@ -1,15 +1,9 @@
 import * as Koa from 'koa';
-import * as bodyParser from 'koa-bodyparser';
 
 export default class FireCat {
     public koa: Koa;
 
     constructor() {
         this.koa = new Koa();
-        this.koa.use(bodyParser())
-        this.koa.on('error', (err) => {
-            console.log('[ERROR]')
-            console.log(err)
-        })
     }
 }
